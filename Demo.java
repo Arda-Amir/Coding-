1d2eaf3e26ed21c8b26a3ff5b01734480f188969
Arda-Amir <ardaamirrashid@gmail.com>
2025-09-17 14:32:54 +0500

    Push Demo.java to GitHub

---
+++ Demo.java
@@ -0,0 +1,16 @@
+//Arda Amir Rashid
+//SP25-BCS-013
+
+
+
+public class Demo{
+public static void main(String[] args){
+
+Student s=new Student("Arda","sp25@gmail.com",3.8);
+s.setname("amal");
+s.setemail("sp2@mail.com");
+s.setGPA(3.2);
+s.display();
+}
+
+}
